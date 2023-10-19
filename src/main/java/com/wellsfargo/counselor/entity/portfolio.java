446598cdf760636abb72a5f56d9bef9a9a3f8b53
Laith Darras <laith.s.darras@gmail.com>
2023-10-19 15:02:05 -0400

    Created new skeleton files for 3 more categories

---
+++ src/main/java/com/wellsfargo/counselor/entity/portfolio.java
@@ -0,0 +1,12 @@
+package com.wellsfargo.counselor.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Portfolio {
+    
+}
