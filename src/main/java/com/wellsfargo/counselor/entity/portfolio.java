a10234c40c74826aee352a273b7439ea2c4babb5
Laith Darras <laith.s.darras@gmail.com>
2023-10-19 15:38:51 -0400

    Implemented 3 more categories

--- src/main/java/com/wellsfargo/counselor/entity/portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/portfolio.java
@@ -8,5 +8,70 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Portfolio {
-    
+    @Id
+    @GeneratedValue
+    private long portId;
+
+    @ManyToOne
+    @JoinColumn(name="clientId")
+    private Client client;
+
+    @Column(nullable=false)
+    private String firstName;
+
+    @Column(nullable=false)
+    private String lastName;
+
+    @Column(nullable=false)
+    private String riskFactor;
+
+    @Column(nullable=false)
+    private String total;
+
+    protected Portfolio {
+
+    }
+
+    public Portfolio(String firstName, String lastName, String riskFactor, String total) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.riskFactor = riskFactor;
+        this.total = total;
+    }
+
+    public Client getPortId() {
+        return portId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRiskFactor() {
+        return riskFactor;
+    }
+
+    public void setRiskFactor(String riskFactor) {
+        this.riskFactor = riskFactor;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
 }
--- src/main/java/com/wellsfargo/counselor/entity/security.java
+++ src/main/java/com/wellsfargo/counselor/entity/security.java
@@ -8,5 +8,82 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Security {
-    
+    @Id
+    @GeneratedValue
+    private long securityId;
+
+    @ManyToOne
+    @JoinColumn(name="portId")
+    private Portfolio portfolio; // object approach rather than long
+
+    @Column(nullable=false)
+    private String symbol;
+
+    @Column(nullable=false)
+    private String type;
+
+    @Column(nullable=false)
+    private String quantity;
+
+    @Column(nullable=false)
+    private String price;
+
+    @Column(nullable=false)
+    private String total;
+
+    protected Client {
+
+    }
+
+    public Client(String symbol, String type, String quantity, String price, String total) {
+        this.symbol = symbol;
+        this.type = type;
+        this.quantity = quantity;
+        this.price = price;
+        this.total = total;
+    }
+
+    public Portfolio getSecurityId() {
+        return securityId;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getTotal() {
+        return email;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
 }
